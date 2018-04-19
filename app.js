3ad293ee05e69545948b46a11a1580f442f3a0ff
Kishikawa Katsumi <kishikawakatsumi@mac.com>
2018-04-20 00:34:51 +0900

    Add edit feature

--- app.js
+++ app.js
@@ -17,16 +17,65 @@ const latestVersion = availableVersions[0];
 const stableVersion = '4.1';
 const maxLength = 1950;
 
+const resultMessages = {}
+
 client.on("ready", () => {
   console.log("Swift compiler bot is ready!");
 });
 
 client.on("message", (message) => {
-  if (!message.isMentioned(client.user) || message.author.bot) {
-    return
-  }
+  processMessage(message)
+});
 
-  const command = message.cleanContent.replace(/@swiftbot/g, '').trim();
+client.on("messageUpdate", (oldMessage, newMessage) => {
+  const message = resultMessages[oldMessage.id]
+  if (message) {
+    if (message._errorMessage) {
+      message._errorMessage.delete();
+    }
+    message['_updatedMessage'] = newMessage
+    processMessage(message)
+  }
+});
+
+client.on("messageDelete", (oldMessage, newMessage) => {
+  const message = resultMessages[oldMessage.id]
+  if (message) {
+    if (message._errorMessage) {
+      message._errorMessage.delete();
+    }
+    if (message._versionMessage) {
+      message._versionMessage.delete();
+    }
+    if (message._stdoutMessage) {
+      message._stdoutMessage.delete();
+    }
+    if (message._stdoutAttachment) {
+      message._stdoutAttachment.delete();
+    }
+    if (message._stderrMessage) {
+      message._stderrMessage.delete();
+    }
+    if (message._stderrAttachment) {
+      message._stderrAttachment.delete();
+    }
+  }
+});
+
+client.login(config.token);
+
+function processMessage(message) {
+  let updateMessages = {};
+  if (message._updatedMessage) {
+    updateMessages = message;
+    message = message._updatedMessage;
+  }
+  if (!message.isMentioned(client.user) || message.author.bot) {
+    return;
+  }
+  const content = message.cleanContent;
+
+  const command = content.replace(/@swiftbot/g, '').trim();
   if (command == 'help' || command == '') {
     showHelp(message)
     return;
@@ -41,13 +90,15 @@ client.on("message", (message) => {
   }
 
   const regex = /```[a-zA-Z]*\n([\s\S]*?\n)```/;
-  const match = regex.exec(message.content);
+  const match = regex.exec(content);
   if (!match) {
     return
   }
 
+  resultMessages[message.id] = {};
+
   const code = match[1];
-  const args = message.content.split('\n');
+  const args = content.split('\n');
   let parsedArguments = {};
   if (args.length > 0) {
     parsedArguments = require('yargs-parser')(args[0]);
@@ -60,14 +111,16 @@ client.on("message", (message) => {
   const defaultCommand = 'swift';
   let swiftCommand = parsedArguments.command || defaultCommand;
   if (!['swift', 'swiftc'].includes(swiftCommand)) {
-    message.channel.send(`⚠️ Command '${swiftCommand}' is not supported.`);
+    message.channel.send(`⚠️ Command '${swiftCommand}' is not supported.`)
+      .then(sent => resultMessages[message.id]['_errorMessage'] = sent);
     return;
   }
 
   let options = parsedArguments.options || '';
   const commandInjectionOperators = [';', '&', '&&', '||', '`', '(', ')', '#'];
   if (commandInjectionOperators.some(operator => options.includes(operator))) {
-    message.channel.send('⚠️ Invalid control characters found.');
+    message.channel.send('⚠️ Invalid control characters found.')
+      .then(sent => resultMessages[message.id]['_errorMessage'] = sent);
     return;
   }
   if (options.length == 0 && swiftCommand == defaultCommand && version == stableVersion) {
@@ -84,16 +137,15 @@ client.on("message", (message) => {
     timeout = maxTimeout;
   }
 
-  versions.forEach(function(version) {
+  versions.forEach(version => {
     if (!availableVersions.includes(version.toString())) {
-      message.channel.send(`⚠️ Swift '${version}' toolchain is not supported.`);
+      message.channel.send(`⚠️ Swift '${version}' toolchain is not supported.`)
+        .then(sent => resultMessages[message.id]['_errorMessage'] = sent);
       return;
     }
-    post(message, code, version, swiftCommand, options, timeout);
+    post(message, code, version, swiftCommand, options, timeout, updateMessages);
   });
-});
-
-client.login(config.token);
+}
 
 function showHelp(message) {
   message.channel.send(`
@@ -129,12 +181,7 @@ Subcommands:
 }
 
 function showVersions(message) {
-  message.channel.send(`
-Available Swift versions:
-\`\`\`
-${availableVersions.join('\n')}
-\`\`\`
-    `.trim());
+  message.channel.send(formatAsCodeBlock(`${availableVersions.join('\n')}`));
 }
 
 function showContribute(message) {
@@ -165,7 +212,11 @@ function parseVersionArgument(argument) {
   return Array.prototype.concat.apply([], versions);
 }
 
-function post(message, code, version, command, options, timeout) {
+function formatAsCodeBlock(text) {
+  return `\`\`\`\n${text}\`\`\``.trim()
+}
+
+function post(message, code, version, command, options, timeout, updateMessages) {
   const request = require("sync-request");
   const res = request('POST', 'https://swift-playground.kishikawakatsumi.com/run', {
     headers: {
@@ -175,88 +226,146 @@ function post(message, code, version, command, options, timeout) {
   });
 
   if (res.statusCode != 200) {
-    message.channel.send(`❗️ Server error: ${res.statusCode}`);
+    message.channel.send(`❗️Server error: ${res.statusCode}`)
+      .then(sent => resultMessages[message.id]['_errorMessage'] = sent);
+    return;
   }
   try {
     const results = JSON.parse(res.body);
     if (results.version) {
       if (version == latestVersion) {
-        sendVersion(message, results.version, `swift-DEVELOPMENT-SNAPSHOT-${latestVersion}`)
+        sendVersion(message, results.version, `swift-DEVELOPMENT-SNAPSHOT-${latestVersion}`, updateMessages)
       } else {
-        sendVersion(message, results.version)
+        sendVersion(message, results.version, null, updateMessages)
       }
     }
     if (results.output) {
-      sendStdout(message, results.output)
+      sendStdout(message, results.output, updateMessages);
+    } else {
+      if (updateMessages._stdoutMessage) {
+        updateMessages._stdoutMessage.delete();
+        resultMessages[message.id]['_stdoutMessage'] = null;
+      }
+      if (updateMessages._stdoutAttachment) {
+        updateMessages._stdoutAttachment.delete();
+        resultMessages[message.id]['_stdoutAttachment'] = null;
+      }
     }
     if (results.errors) {
-      sendStderr(message, results.errors)
+      sendStderr(message, results.errors, updateMessages);
+    } else {
+      if (updateMessages._stderrMessage) {
+        updateMessages._stderrMessage.delete();
+        resultMessages[message.id]['_stderrMessage'] = null;
+      }
+      if (updateMessages._stderrAttachment) {
+        updateMessages._stderrAttachment.delete();
+        resultMessages[message.id]['_stderrAttachment'] = null;
+      }
     }
   } catch (e) {
-    message.channel.send(`❗️ Invalid JSON returned.`);
+    console.log(e);
+    message.channel.send(`❗️Invalid JSON returned.`)
+      .then(sent => resultMessages[message.id]['_errorMessage'] = sent);
   }
 }
 
-function sendVersion(message, version, snapshotVersion) {
+function sendVersion(message, version, snapshotVersion, updateMessages) {
   const versionLines = version.split('\n')
   let versionString = version
   if (versionLines.length > 0) {
     versionString = versionLines[0]
   }
+  let content = ""
   if (snapshotVersion) {
-    message.channel.send(`
-\`\`\`
-${snapshotVersion}
-${versionString}
-\`\`\`
-      `.trim());
+    content = formatAsCodeBlock(`${snapshotVersion}\n${versionString}`);
   } else {
-    message.channel.send(`
-\`\`\`
-${versionString}
-\`\`\`
-      `.trim());
+    content = formatAsCodeBlock(`${versionString}`);
+  }
+
+  if (updateMessages._versionMessage) {
+    updateMessages._versionMessage.edit(content)
+      .then(sent => resultMessages[message.id]['_versionMessage'] = sent);
+  } else {
+    message.channel.send(content)
+      .then(sent => resultMessages[message.id]['_versionMessage'] = sent);
   }
 }
 
-function sendStdout(message, output) {
+function sendStdout(message, output, updateMessages) {
   if (output.length <= maxLength) {
-    message.channel.send(`
-\`\`\`
-${output}
-\`\`\`
-      `.trim());
+    const content = formatAsCodeBlock(`${output}`);
+    if (updateMessages._stdoutMessage) {
+      updateMessages._stdoutMessage.edit(content)
+        .then(sent => resultMessages[message.id]['_stdoutMessage'] = sent);
+      if (updateMessages._stdoutAttachment) {
+        updateMessages._stdoutAttachment.delete();
+        resultMessages[message.id]['_stdoutAttachment'] = null;
+      }
+    } else {
+      message.channel.send(content)
+        .then(sent => resultMessages[message.id]['_stdoutMessage'] = sent);
+    }
   } else {
     const messages = Util.splitMessage(output);
     if (Array.isArray(messages) && messages.length > 0) {
-      message.channel.send(`
-\`\`\`
-${messages[0]}
-...
-\`\`\`
-        `.trim());
+      const content = formatAsCodeBlock(`${messages[0]}\n...`);
+      const attachment = {files: [{attachment: Buffer.from(output, 'utf8'), name: 'stdout.txt'}]};
+      if (updateMessages._stdoutMessage) {
+        updateMessages._stdoutMessage.edit(content)
+          .then(sent => resultMessages[message.id]['_stdoutMessage'] = sent);
+        if (updateMessages._stdoutAttachment) {
+          updateMessages._stdoutAttachment.edit(attachment)
+            .then(sent => resultMessages[message.id]['_stdoutAttachment'] = sent);
+        } else {
+          message.channel.send(attachment)
+            .then(sent => resultMessages[message.id]['_stdoutAttachment'] = sent);
+        }
+      } else {
+        message.channel.send(content)
+          .then(sent => resultMessages[message.id]['_stdoutMessage'] = sent);
+        message.channel.send(attachment)
+          .then(sent => resultMessages[message.id]['_stdoutAttachment'] = sent);
+      }
     }
-    message.channel.send({files: [{attachment: Buffer.from(output, 'utf8'), name: 'stdout.txt'}]})
   }
 }
 
-function sendStderr(message, errors) {
+function sendStderr(message, errors, updateMessages) {
   if (errors.length <= maxLength) {
-    message.channel.send(`
-\`\`\`
-${errors}
-\`\`\`
-      `.trim());
-} else {
-  const messages = Util.splitMessage(errors);
-  if (Array.isArray(messages) && messages.length > 0) {
-    message.channel.send(`
-\`\`\`
-${messages[0]}
-...
-\`\`\`
-        `.trim());
+    const content = formatAsCodeBlock(`${errors}`);
+    if (updateMessages._stderrMessage) {
+      updateMessages._stderrMessage.edit(content)
+        .then(sent => resultMessages[message.id]['_stderrMessage'] = sent);
+      if (updateMessages._stderrAttachment) {
+        updateMessages._stderrAttachment.delete();
+        resultMessages[message.id]['_stderrAttachment'] = null;
+      }
+    } else {
+      message.channel.send(content)
+        .then(sent => resultMessages[message.id]['_stderrMessage'] = sent);
     }
-    message.channel.send({files: [{attachment: Buffer.from(errors, 'utf8'), name: 'stderr.txt'}]})
+  } else {
+    const messages = Util.splitMessage(errors);
+    if (Array.isArray(messages) && messages.length > 0) {
+      const content = formatAsCodeBlock(`${messages[0]}\n...`);
+      const attachment = {files: [{attachment: Buffer.from(errors, 'utf8'), name: 'stderr.txt'}]};
+      if (updateMessages._stderrMessage) {
+        updateMessages._stderrMessage.edit(content)
+          .then(sent => resultMessages[message.id]['_stderrMessage'] = sent);
+        if (updateMessages._stderrAttachment) {
+          updateMessages._stderrAttachment.edit(attachment)
+            .then(sent => resultMessages[message.id]['_stderrAttachment'] = sent);
+        } else {
+          message.channel.send(attachment)
+            .then(sent => resultMessages[message.id]['_stderrAttachment'] = sent);
+        }
+      } else {
+        message.channel.send(content)
+          .then(sent => resultMessages[message.id]['_stderrMessage'] = sent);
+        message.channel.send(attachment)
+          .then(sent => resultMessages[message.id]['_stderrAttachment'] = sent);
+      }
+    }
   }
 }
